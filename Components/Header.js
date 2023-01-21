4609de837a7545b920d6a42b6f19dca519e77794
Jade Ferguson <jadesnider2440@gmail.com>
2023-01-21 14:53:40 -0800

    Added some styling to header

--- Components/Header.js
+++ Components/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Image from 'next/legacy/image';
 
+
 function Header() {
   return (
-    <header>
+    <header className='sticky top-0'>
         {/*left */}
-        <div className="relative flex items-center h-10 cursor-pointer">
+        <div className='relative flex items-center h-10 cursor-pointer
+            my-auto'>
             <Image
                 src='https://links.papareact.com/qd3'
                 alt=""
                 layout='fill'
-                objectFit='contain' // Will keep object ratio
-                objectPosition='left'
+                object-fit='contain' // Will keep object ratio
+                object-position='left'
             />
         </div>
 

c832e3bc79a9c3149db29fdaf84a97d6a6fa446f
Jade Ferguson <jadesnider2440@gmail.com>
2023-02-10 15:52:36 -0800

    After search dates are displayed on search page

--- pages/search.js
+++ pages/search.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import { useRouter } from 'next/router';
+import {format} from "date-fns";
 
 // Great thing about components with react you can use them across different pages
 function Search() {
+    const router = useRouter();
+
+    // es6 destructuring
+    const {location, startDate, endDate, noOfGuests} = router.query;
+    const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+    const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+    const range = `${formattedStartDate} - ${formattedEndDate}`;
+ 
   return (
     <div>
         <Header />
 
         <main className="flex">
             <section className="flex-grow pt-14 px-6">
-                <p className="text-xs">300+ stays for 5 number of guests</p>
+                <p className="text-xs">300+ stays - {range} - for {noOfGuests} guests</p>
 
-                <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+                <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in {location}</h1>
 
                 <div className="hidden lg:inline-flex mb-5 space-x-3
                 text-gray-800 whitespace-nowrap">

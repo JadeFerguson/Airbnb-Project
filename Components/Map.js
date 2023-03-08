671876c124c7c46d6ca2626fc48fb3f45272237e
Jade Ferguson <jadesnider2440@gmail.com>
2023-03-08 13:34:11 -0800

    Mapbox shows are of searched location

--- Components/Map.js
+++ Components/Map.js
@@ -1,23 +1,32 @@
 import { useState } from 'react';
 import ReactMapGL from 'react-map-gl';
+import getCenter from "geolib/es/getCenter";
 
-function Map() {
-  const [viewport, setViewport] = useState({
+function Map({searchResults}) {
+  
+// Transform the search results into the object we require
+  const coordinates = searchResults.map(result => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+  const center = getCenter(coordinates);
+
+    const [viewport, setViewport] = useState({
       width: "100%",
-      height: "100%",
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoo: 11,
-  });
+      length: "100%",
+      latitude: center.latitude,
+      longitude: center.longitude,
+      zoom: 11,
+    });
 
-
-    return <ReactMapGL
+    return (<ReactMapGL
       mapStyle='mapbox://styles/jadesnider/clf039mcm000t01ob6xj58pn4'
       mapboxAccessToken={process.env.mapbox_key}
       {...viewport}
-
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
     >    
     </ReactMapGL>
+    );
 }
 
 export default Map;
